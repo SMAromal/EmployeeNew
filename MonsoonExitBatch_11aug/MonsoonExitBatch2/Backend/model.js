abab1eab66ea2b7088ab7219a410d13dfdc2226d
SMAromal <119115189+SMAromal@users.noreply.github.com>
2024-08-11 11:46:46 +0530

    Update model.js

--- MonsoonExitBatch_11aug/MonsoonExitBatch2/Backend/model.js
+++ MonsoonExitBatch_11aug/MonsoonExitBatch2/Backend/model.js
@@ -6,8 +6,5 @@ const schema = mongoose.Schema({
   img_url: String,
 });
 
-// Create a model from the schema
 const Post = mongoose.model('Post', schema);
-
-// Export the model
 module.exports = Post;
